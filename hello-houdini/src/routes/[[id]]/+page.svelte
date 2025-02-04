bd6b0653dc8578b03cf63c6df8559a8666f74a30
youpiwaza <masamune.code@gmail.com>
2025-02-04 11:09:24 +0100

    MOD: Reusing Parts of a Query

--- hello-houdini/src/routes/[[id]]/+page.svelte
+++ hello-houdini/src/routes/[[id]]/+page.svelte
@@ -1,5 +1,6 @@
 <script>
     import { Container, Display, Sprite, Panel } from '~/components'
+    import { SpeciesPreview, SpeciesPreviewPlaceholder } from '~/components'
     
     /* @type { import('./$houdini').PageData } */
     export let data
@@ -18,8 +19,7 @@
         </Display>
         <Sprite
             id="species-sprite"
-            src={$Info.data.species.sprites.front}
-            speciesName={$Info.data.species.name}
+            species={$Info.data.species}
         />
         <Display id="species-flavor_text">
             {$Info.data.species.flavor_text}
@@ -34,6 +34,15 @@
                 next
             </a>
         </nav>
+        <div id="species-evolution-chain">
+            {#each $Info.data.species.evolution_chain as form, i}
+                <SpeciesPreview species={form} number={i + 1} />
+            {/each}
+            <!-- if there are less than three species in the chain, leave a placeholder behind -->
+            {#each Array.from({ length: 3 - $Info.data.species.evolution_chain?.length }) as _, i}
+                <SpeciesPreviewPlaceholder number={$Info.data.species.evolution_chain.length + i + 1} />
+            {/each}
+        </div>
     </Panel>
 </Container>
 {/if}
